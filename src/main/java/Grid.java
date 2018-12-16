7eaed5f15d9b340ad60f700fa575b5f5253a415c
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2018-12-16 16:52:00 +0200

    Replace Canvas with Pane.

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,34 +1,44 @@
 package main.java;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 import java.util.ArrayList;
 
-public class Grid extends Region {
+public class Grid extends BorderPane {
     public Grid() {
-        Canvas canvas = new Canvas(500, 500);
-        this.getChildren().add(canvas);
+        Pane canvas = new Pane();
+        canvas.setPrefSize(500, 500);
+        this.setCenter(canvas);
         canvas.setOnMouseClicked(canvasOnMouseClicked);
     }
 
-    private final double radius = 10;
+    private final double radius = 5;
+    private ArrayList<Point> points = new ArrayList<>();
+    private ArrayList<Circle> circles = new ArrayList<>();
 
     private EventHandler<MouseEvent> canvasOnMouseClicked = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent e) {
-            if (e.getSource() instanceof Canvas) {
-                Canvas canvas = (Canvas)e.getSource();
-                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+            if (e.getSource() instanceof Pane) {
+                Pane canvas = (Pane)e.getSource();
 
                 double x = e.getX();
                 double y = e.getY();
-                graphicsContext.setFill(Color.BLACK);
-                graphicsContext.fillOval(x - radius, y - radius, radius, radius);
+                Circle newPoint = new Circle(x, y, radius);
+                newPoint.setFill(Color.BLACK);
+                circles.add(newPoint);
+                canvas.getChildren().add(newPoint);
             }
         }
     };
